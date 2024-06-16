43aafe3e612bbb90eaef5c68c631096e49f14e77
Lior Shahar <lior2kx@gmail.com>
2024-06-16 15:21:59 +0300

    techstack code improvement

--- src/components/TechStack/TechCarousel.js
+++ src/components/TechStack/TechCarousel.js
@@ -19,7 +19,14 @@ const TechCarousel = ({ data, ltr = false }) => {
         rtl: ltr,
         responsive: [
             {
-                breakpoint: 1100,
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 4,
                     slidesToScroll: 1,
@@ -29,13 +36,6 @@ const TechCarousel = ({ data, ltr = false }) => {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
                     slidesToScroll: 1,
                 },
             },
--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -5,12 +5,11 @@ import TechStackData from './techStackData';
 
 const TechStackContent = () => {
     return (
-        // <div className='tech-stack fade-in'>
         <div className='tech-stack'>
             <h1 className='tech-stack-title'>My Tech Stack</h1>
-            <TechCarousel data={TechStackData[0]}></TechCarousel>
-            <TechCarousel data={TechStackData[1]} ltr={true}></TechCarousel>
-            <TechCarousel data={TechStackData[2]}></TechCarousel>
+            {TechStackData.map((data, index) => (
+                <TechCarousel key={index} data={data} ltr={index % 2 === 1} />
+            ))}
         </div>
     );
 };
